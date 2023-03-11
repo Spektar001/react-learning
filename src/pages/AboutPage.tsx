4cb67280b9457fd765cc7a73eed9aaadddbeabff
Harry-Gupenec <Spektar001@mail.ru>
2023-03-11 13:23:50 +0300

    feat: add react educational project on TS from Vladilen Minin

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque eius quos ducimus corporis! Mollitia est rerum corrupti, perferendis, omnis illum quos autem, ullam possimus quidem officiis eveniet assumenda quam iste?
+    </div>
+  )
+}
